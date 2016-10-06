35137b54f593c39dbe5ff9db5f455cb4bc833b14
Mary Scale <scalemary@gmail.com>
2016-10-06 18:25:37 +0000

    Finished the get positives function

--- answers.js
+++ answers.js
@@ -18,3 +18,24 @@ printPos([-1, 10, 9, -4, 0, 3]);
 
 
 //Get positives 
+//get positives function using filter method
+function getPos(array){
+    var posNums = [];
+    //have a new empty array ready before filtering
+        //must be outside of the filter
+    array.filter(function(value) {
+        if(value >= 0){
+           posNums.push(value);
+        };
+        //always have a boolean test for filters
+    })
+    //end of filter function for finding positive values
+    return posNums;
+    //return our filled up new array that has the filtered data
+        //make sure this is outside of the filter function!
+};
+getPos([-10,10,-5,5,4]);
+
+
+
+//
